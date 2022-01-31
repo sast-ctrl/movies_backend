69f920017a9f159a9ceb448e44369e749db0188e
sast-ctrl <81892179+sast-ctrl@users.noreply.github.com>
2022-01-31 01:04:04 -0600

    JWT Token

--- movie_api/urls.py
+++ movie_api/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from .views import MovieLists, MovieDetails#, MovieListView
+from rest_framework_simplejwt import views as jwt_views
 
 urlpatterns = [
     path('movies/', MovieLists.as_view()),
     path('movies/<slug:slug>/', MovieDetails.as_view(), name='movie_detail'),
 
+    path('token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
     # path('api_movies/', MovieListView.as_view(), name='movie_details'),
     # path('api_movies/<slug:slug>', MovieDetail.as_view())
 ]
